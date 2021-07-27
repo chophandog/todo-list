078d854d7ed01a56520f87e458ffc022bbae8009
Your Name <chophandog@gmail.com>
2021-07-27 21:20:42 +0700

    created Expenses, Expense Item, Expense Date and Card

---
+++ src/components/expense-item/expense-item.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Card from "../card/card.component";
+import ExpenseDate from "../expense-date/expense-date.component";
+
+import "./expense-item.styles.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
